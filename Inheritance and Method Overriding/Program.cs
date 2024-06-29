d9c7fa5ed4ed4cfede2835b42fb4c7b4a9540a25
LATIFIBRAHIM <librahim010@st.ug.edu.gh>
2024-06-29 23:23:42 +0000

    Inheritance and Method Overriding

---
+++ Inheritance and Method Overriding/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+// Inheritance and Method Overriding
+
+    class Animal
+    {
+        public virtual void MakeSound()
+        {
+            Console.WriteLine("Some generic sound");
+        }
+    }
+
+    class Dog : Animal
+    {
+        public override void MakeSound()
+        {
+            Console.WriteLine("Bark");
+        }
+    }
+
+    class Cat : Animal
+    {
+        public override void MakeSound()
+        {
+            Console.WriteLine("Meow");
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Animal myAnimal = new Animal();
+            Animal myDog = new Dog();
+            Animal myCat = new Cat();
+
+            myAnimal.MakeSound();
+            myDog.MakeSound();
+            myCat.MakeSound();
+        }
+    }
